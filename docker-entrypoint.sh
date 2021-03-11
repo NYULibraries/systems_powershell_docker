fadcf0b11df5b8b0f1da9d192891bfb8db02fd67
Eric Griffis <eric.griffis@nyu.edu>
2021-03-11 13:33:42 -0500

    Fix docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,3 +3,5 @@
 sed -i "s|\$SERVER_NAME|$SERVER_NAME|g" illiad.iis.reset.ps1  
 sed -i "s|\$POWERSHELL_CRED|$POWERSHELL_CRED|g" illiad.iis.reset.ps1  
 sed -i "s|\$POWERSHELL_PW|$POWERSHELL_PW|g" illiad.iis.reset.ps1  
+
+exec "$@"
