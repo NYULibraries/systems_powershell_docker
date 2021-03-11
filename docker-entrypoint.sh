e54163533ee26c82ed266aa398008267048a1aae
Eric Griffis <eric.griffis@nyu.edu>
2021-03-11 12:16:11 -0500

    Fix entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #/bin/sh -e
 
-sed -i "s|\$SERVER_NAME|$SERVER_NAME|g" /etc/shibboleth/shibboleth2.xml  
-sed -i "s|\$POWERSHELL_CRED|$POWERSHELL_CRED|g" /etc/shibboleth/shibboleth2.xml  
-sed -i "s|\$POWERSHELL_PW|$POWERSHELL_PW|g" /etc/shibboleth/shibboleth2.xml  
+sed -i "s|\$SERVER_NAME|$SERVER_NAME|g" illiad.iis.reset.ps1  
+sed -i "s|\$POWERSHELL_CRED|$POWERSHELL_CRED|g" illiad.iis.reset.ps1  
+sed -i "s|\$POWERSHELL_PW|$POWERSHELL_PW|g" illiad.iis.reset.ps1  
