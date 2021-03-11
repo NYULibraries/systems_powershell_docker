5323f5e0695eef65e1f28d18ebad200c332974e4
Eric Griffis <eric.griffis@nyu.edu>
2021-03-11 13:37:06 -0500

    Change username environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 sed -i "s|\$SERVER_NAME|$SERVER_NAME|g" illiad.iis.reset.ps1  
-sed -i "s|\$POWERSHELL_CRED|$POWERSHELL_CRED|g" illiad.iis.reset.ps1  
+sed -i "s|\$POWERSHELL_USER|$POWERSHELL_USER|g" illiad.iis.reset.ps1  
 sed -i "s|\$POWERSHELL_PW|$POWERSHELL_PW|g" illiad.iis.reset.ps1  
 
 exec "$@"
