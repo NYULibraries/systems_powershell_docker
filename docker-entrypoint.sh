fe6a02bf830bb6d892717f024a28095e0b4806a6
Systems-NYULib <lib-systems@nyu.edu>
2021-03-12 14:43:20 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 sed -i "s|\$SERVER_NAME|$SERVER_NAME|g" illiad.iis.reset.ps1  
 sed -i "s|\$POWERSHELL_USER|$POWERSHELL_USER|g" illiad.iis.reset.ps1  
 sed -i "s|\$POWERSHELL_PW|$POWERSHELL_PW|g" illiad.iis.reset.ps1  
+sed -i "s|\$SERVER_NAME|$SERVER_NAME|g" illiad.services.reset.ps1  
+sed -i "s|\$POWERSHELL_USER|$POWERSHELL_USER|g" illiad.services.reset.ps1  
+sed -i "s|\$POWERSHELL_PW|$POWERSHELL_PW|g" illiad.services.reset.ps1  
 
 exec "$@"
